61388bc21df73d4e1136852c8345b30b5b582413
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 22:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
   // write your code here
+	if(text.length < 1) return "";
+	return `${text.toLowerCase()}-${text.toUpperCase()}`
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
 alert(toCase(text));
+
